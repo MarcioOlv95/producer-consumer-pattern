1d77b4b4517444d0328f6206bf0af2fde124e34e
MarcioOlv95 <marcio.oliveira1995@hotmail.com>
2025-05-24 00:18:02 -0300

    Add project files.

---
+++ src/producer-consumer-pattern/Client.cs
@@ -0,0 +1,94 @@
+﻿using System.Text.Json.Serialization;
+
+namespace producer_consumer_pattern;
+
+/// <summary>
+/// Order is a json-friendly representation of an order.
+/// </summary>
+/// <param name="Id">order id</param>
+/// <param name="Name">food name</param>
+/// <param name="Temp">ideal temperature</param>
+/// <param name="Freshness">freshness in seconds</param>
+record Order(string Id, string Name, string Temp, long Freshness);
+
+record Problem(List<Order> Orders);
+
+
+/// <summary>
+/// Action is a json-friendly representation of an action.
+/// </summary>
+/// <param name="timestamp">action timestamp</param>
+/// <param name="id">order id</param>
+/// <param name="action">place, move, pickup or discard</param>
+class Action(DateTime timestamp, string id, string action)
+{
+    public static readonly string Place = "place";
+    public static readonly string Move = "move";
+    public static readonly string Pickup = "pickup";
+    public static readonly string Discard = "discard";
+
+    public DateTime DateTime { get; init; } = timestamp;
+    [JsonPropertyName("timestamp")]
+    public long Timestamp { get; init; } = (long)timestamp.Subtract(DateTime.UnixEpoch).TotalMicroseconds;
+    [JsonPropertyName("id")]
+    public string Id { get; init; } = id;
+    [JsonPropertyName("action")]
+    public string Action_ { get; init; } = action;
+};
+
+class Client()
+{
+    public async Task<Problem> NewProblemAsync()
+    {
+        return new Problem([
+            new("kc3yd", "Chicken Nuggets", "hot", 109),
+            new("k6ie1", "Pressed Juice", "cold", 166),
+            new("oozhn", "Tuna Sandwich", "cold", 76),
+            new("ye5h4", "Stale Bread", "room", 84),
+            new("97kyd", "Coconut", "room", 68),
+            new("6xt88", "French Fries", "hot", 70),
+            new("pqi6y", "Tuna Sandwich", "cold", 142),
+            new("k33xb", "Apple", "room", 154),
+            new("cnhek", "Mixed Greens", "cold", 155),
+            new("b8dmo", "Pressed Juice", "cold", 114),
+            new("8foed", "Vanilla Ice Cream", "cold", 92),
+            new("jqwjk", "Banana", "room", 117),
+            new("qd6ci", "Chocolate Gelato", "cold", 152),
+            new("p7jc7", "Sushi", "cold", 132),
+            new("99xpj", "Tuna Sandwich", "cold", 171),
+            new("ytwba", "Chocolate Gelato", "cold", 132),
+            new("3f13a", "Danish Pastry", "room", 172),
+            new("q6jf6", "Banana", "room", 133),
+            new("cskmf", "Sushi", "cold", 109),
+            new("r7o5a", "Italian Meatballs", "hot", 95),
+            new("jw6m4", "Tomato Soup", "hot", 89),
+            new("c4h6o", "Lasagna", "hot", 77),
+            new("g6ze6", "Cheeseburger", "hot", 155),
+            new("1sdcq", "Tuna Sandwich", "cold", 65),
+            new("53qyj", "Vanilla Ice Cream", "cold", 157),
+            new("w8cq3", "Pad Thai", "hot", 76),
+            new("fdsim", "Chicken Tacos", "hot", 150),
+            new("z97b6", "Kale Salad", "cold", 133),
+            new("rh7ip", "Pad See Ew", "hot", 104),
+            new("utu8w", "Apple", "room", 63),
+            new("84qq5", "Vanilla Ice Cream", "cold", 161),
+            new("1yiey", "Mac & Cheese", "hot", 84),
+            new("osxjk", "Bacon Burger", "hot", 106),
+            new("kc88d", "Stale Bread", "room", 160),
+            new("mad8w", "Cheeseburger", "hot", 178),
+            new("5swzx", "Mac & Cheese", "hot", 162),
+            new("e33hj", "Burrito", "hot", 106),
+            new("cj1do", "BBQ Pizza", "hot", 95),
+            new("x9h4b", "Gas Station Sushi", "room", 127),
+            new("wscrq", "Turkey Sandwich", "cold", 92),
+            new("o7dtq", "Chicken Nuggets", "hot", 107),
+            new("k6jqj", "Fizzed-Out Pepsi", "room", 109),
+            new("3xido", "Pad Thai", "hot", 114),
+            new("tjnrp", "Vegetarian Pizza", "hot", 139),
+            new("4rrjc", "Lasagna", "hot", 141),
+            new("qi5wo", "Cheese Pizza", "hot", 176),
+            new("zdpz6", "Danish Pastry", "room", 121),
+            new("bu5d4", "Tuna Sandwich", "cold", 64)
+        ]);
+    }
+}
---
+++ src/producer-consumer-pattern/Main.cs
@@ -0,0 +1,184 @@
+﻿using System.Threading.Channels;
+
+namespace producer_consumer_pattern;
+
+static class ProducerConsumerPattern
+{
+    private static readonly string hot = "hot", cold = "cold", room = "room";
+    private static readonly int amountTotalHeater = 6, amountTotalCooler = 6, amountTotalShelf = 12;
+    private static readonly List<Order> itemsCold = [];
+    private static readonly List<Order> itemsHot = [];
+    private static readonly List<Order> itemsShelf = [];
+
+    /// <summary>
+    /// Challenge harness
+    /// </summary>
+    /// <param name="min">Minimum pickup time (in seconds)</param>
+    /// <param name="max">Maximum pickup time (in seconds)</param>
+    static async Task Main(int min = 4, int max = 8)
+    {
+        try
+        {
+            var client = new Client();
+            var problem = await client.NewProblemAsync();
+
+            var channel = Channel.CreateBounded<Action>(new BoundedChannelOptions(problem.Orders.Count)
+            {
+                FullMode = BoundedChannelFullMode.Wait,
+                AllowSynchronousContinuations = true,
+                SingleWriter = false
+            });
+
+            var actions = new List<Action>();
+
+            var producerTask = Producer(channel.Writer, problem.Orders, actions);
+            var consumerTask = Consumer(channel.Reader, actions, min, max);
+
+            await Task.WhenAll(producerTask, consumerTask);
+
+            Console.WriteLine($"End.");
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Simulation failed: {e}");
+        }
+    }
+
+    static async Task Producer(ChannelWriter<Action> writer, List<Order> orders, List<Action> actions)
+    {
+        var countList = orders.Count;
+        var itensPerSecond = 2;
+
+        for (int i = 0; i < countList; i++)
+        {
+            var order = orders[i];
+
+            if (i != 0 && i % itensPerSecond == 0)
+            {
+                await Task.Delay(2000);
+            }
+
+            if (order.Temp.Equals(hot) && itemsHot.Count < amountTotalHeater)
+            {
+                var action = new Action(DateTime.Now, order.Id, Action.Place);
+                actions.Add(action);
+                await writer.WriteAsync(action);
+
+                Console.WriteLine($"Produced: {order.Id} - {DateTime.Now} - {order.Temp} - HOT");
+
+                itemsHot.Add(order);
+                continue;
+            }
+
+            if (order.Temp.Equals(cold) && itemsCold.Count < amountTotalCooler)
+            {
+                var action = new Action(DateTime.Now, order.Id, Action.Place);
+                actions.Add(action);
+                await writer.WriteAsync(action);
+
+                Console.WriteLine($"Produced: {order.Id} - {DateTime.Now} - {order.Temp} - COLD");
+
+                itemsCold.Add(order);
+                continue;
+            }
+
+            if (itemsShelf.Count < amountTotalShelf)
+            {
+                var action = new Action(DateTime.Now, order.Id, Action.Place);
+                actions.Add(action);
+                await writer.WriteAsync(action);
+
+                Console.WriteLine($"Produced: {order.Id} - {DateTime.Now} - {order.Temp} - SHELF");
+
+                itemsShelf.Add(order);
+                continue;
+            } 
+            else
+            {
+                var coldItem = itemsShelf.Find(x => x.Temp.Equals(cold, StringComparison.OrdinalIgnoreCase));
+                if (coldItem is not null && itemsCold.Count < amountTotalCooler)
+                {
+                    itemsCold.Add(coldItem);
+                    itemsShelf.Remove(coldItem);
+                    actions.Add(new Action(DateTime.Now, coldItem.Id, Action.Move));
+                    Console.WriteLine($"Move {order.Name} - {order.Id} - {order.Temp} - {DateTime.Now.ToLongTimeString()}");
+                }
+
+                var hotItem = itemsShelf.Find(x => x.Temp.Equals(hot, StringComparison.OrdinalIgnoreCase));
+                if (hotItem is not null && itemsHot.Count < amountTotalHeater)
+                {
+                    itemsHot.Add(hotItem);
+                    itemsShelf.Remove(hotItem);
+                    actions.Add(new Action(DateTime.Now, hotItem.Id, Action.Move));
+                    Console.WriteLine($"Move {order.Name} - {order.Id} - {order.Temp} - {DateTime.Now.ToLongTimeString()}");
+                }
+
+                if (itemsShelf.Count < amountTotalShelf)
+                {
+                    var action = new Action(DateTime.Now, order.Id, Action.Place);
+                    actions.Add(action);
+                    await writer.WriteAsync(action);
+
+                    Console.WriteLine($"Produced: {order.Id} - {DateTime.Now} - {order.Temp} - SHELF");
+
+                    itemsShelf.Add(order);
+                }
+                else
+                {
+                    itemsShelf.Remove(itemsShelf.First());
+                    itemsShelf.Add(order);
+                    actions.Add(new Action(DateTime.Now, order.Id, Action.Discard));
+
+                    var action = new Action(DateTime.Now, order.Id, Action.Place);
+                    actions.Add(action);
+                    await writer.WriteAsync(action);
+
+                    Console.WriteLine($"Discard:  {itemsShelf.First().Id} - {itemsShelf.First().Temp} - {DateTime.Now.ToLongTimeString()}");
+                    Console.WriteLine($"Produced: {order.Id} - {DateTime.Now} - {order.Temp} - SHELF");
+                }
+            }
+        }
+
+        writer.Complete();
+
+    }
+
+    static async Task Consumer(ChannelReader<Action> reader, List<Action> actions, int min, int max)
+    {
+        while (await reader.WaitToReadAsync())
+        {
+            while (reader.TryRead(out var item))
+            {
+                if (actions.Any(x => x.Id.Equals(item.Id) && x.Action_.Equals("discard")))
+                    continue;
+
+                var delayTime = (long)TimeSpan.FromSeconds(new Random().Next(min, max)).TotalMilliseconds;
+
+                var elapsedTime = DateTime.Now.Subtract(item.DateTime).TotalMilliseconds;
+
+                var timeToWait = delayTime - elapsedTime;
+
+                if (timeToWait > 0) 
+                { 
+                    await Task.Delay((int)timeToWait);
+                }
+
+                actions.Add(new Action(DateTime.Now, item.Id, Action.Pickup));
+                Console.WriteLine($"Consumed: {item.Id} - {DateTime.Now} - random {delayTime}");
+
+                var hotItem = itemsHot.Find(x => x.Id.Equals(item.Id));
+                if (hotItem is not null)
+                    itemsHot.Remove(hotItem);
+
+                var coldItem = itemsCold.Find(x => x.Id.Equals(item.Id));
+                if (coldItem is not null)
+                    itemsCold.Remove(coldItem);
+
+                var roomItem = itemsShelf.Find(x => x.Id.Equals(item.Id));
+                if (roomItem is not null)
+                    itemsShelf.Remove(roomItem);
+            }
+        }
+    }
+}
